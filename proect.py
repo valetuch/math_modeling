34bbe5bc785ab4b4ac6137dc81d404683dbc6d57
X_Xvaletuch <91481760+valetuch@users.noreply.github.com>
2021-12-27 14:59:00 +0000

    fgh

---
+++ proect.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+ 
+fig, ax = plt.subplots()
+line, = plt.plot([], [], '-', color='red', label='line')
+
+edge = 200
+v0=55
+g=9.8
+ 
+plt.axis('equal')
+
+ax.set_xlim(-1, edge)
+ax.set_ylim(-1, edge)
+ 
+x, y = [], []
+
+def animate(frame):
+    t = frame
+    xn=v0*t
+    yn=v0*t-g*t**2/2
+    x.append(xn)
+    y.append(yn)
+    line.set_data(x, y)
+
+ani = animation.FuncAnimation(fig,
+                        animate,
+                        frames=np.arange(0, 10, 0.1),
+                        interval=30)
+
+ani.save('my_anim.gif')
